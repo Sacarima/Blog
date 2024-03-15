3e15fb7c01c74585ad0cbd6a823c967dd698101f
Sacarima <caroline12alex@yahoo.com>
2024-03-15 11:06:24 +0100

    connect to mangoDB database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+//  Load environment variables
+dotenv.config()
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_STRING)
+    .then(() => { 
+        console.log('Connected to MongoDB') 
+    })
+    .catch((err) => {
+         console.log('Error connecting to MongoDB', err) 
+    })
+
+// Create an express application
 const app = express()
 
 
-
+//  Start the server
 app.listen(9000, () => {    
     console.log('Server is running on port 9000')
 })  
