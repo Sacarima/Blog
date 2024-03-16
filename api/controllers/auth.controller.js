d643b52bf371b0154708f57af9aee607c2cd0cd0
Sacarima <caroline12alex@yahoo.com>
2024-03-16 23:54:51 +0100

    create login API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 
 // Create a new user
@@ -46,5 +47,48 @@ export const signup = async (req, res, next) => {
     }
 
     
+}
+
+// Login a user
+
+export const login = async (req, res, next) => {
+    // Get the user data from the request body
+    const { email, password } = req.body
+    // Validate the request body
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'Please fill in all fields'))
+    }
+
+    // Check if the user exists in the database
+    try {
+        const validUser = await User.findOne({
+            email
+        })
+        // If the user does not exist
+        if (!validUser) {
+           return next(errorHandler(404, 'wrong credentials'))
+        }
+        // Compare the password
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return next(errorHandler(400, 'wrong credentials'))
+        }
+
+        // Create a token
+        const token = jwt.sign({
+            _id: validUser._id
+        }, process.env.JWT_SECRET_TOKEN)
+
+        // remove the password from the user object
+        const { password: pass, ...rest } = validUser._doc
+        res
+            .status(200)
+            .cookie('access_token', token , {
+                httpOnly: true,
+            })
+            .json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
 
