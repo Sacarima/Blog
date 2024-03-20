ecf8af582c354c3b9d88d004bcbdf12bb386ab26
Sacarima <caroline12alex@yahoo.com>
2024-03-20 17:50:47 +0100

    complete darkMode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import themeReducer from './theme/themeSlice'
 
 // Combine all reducers
 const rootReducer = combineReducers({
     user: userReducer,
+    theme: themeReducer,
   })
 
 
